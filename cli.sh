8e0295f7b129d18ff514d088394200a1868271b6
Jan Jaap <jan.jaap@digimondo.de>
2024-11-01 18:27:59 +0100

    WIP: add cli.sh to run the cli

---
+++ cli.sh
@@ -0,0 +1,9 @@
+# check if bun is installed, install it if not
+if ! command -v bun &> /dev/null
+then
+  echo "Bun is not installed, installing..."
+  curl -fsSL https://bun.sh/install | bash
+fi
+
+# run the cli using `bun run dev -- <all args passed to the cli>`
+bun run dev -- $@
